92427e5a22eb9297360dd9480a955b4bcfbea68c
Ewan Brain <ewan.brain@gmail.com>
2021-03-02 18:15:29 +0000

    chart container import working

---
+++ src/containers/ChartContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ChartContainer = () => {
+
+    return (
+        <>
+        <p>Chart data!</p>
+        </>
+    )
+
+
+
+
+}
+
+export default ChartContainer;
